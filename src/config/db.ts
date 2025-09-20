2fe1b0040baee76729a7ac1c55a15f10df6d672d
Miguel Tafur <miguelmita03@gmail.com>
2025-09-20 12:08:05 -0300

    Agregando permisos al archivo db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
     models: [__dirname + '/../models/**/*'],
     logging: false
 })

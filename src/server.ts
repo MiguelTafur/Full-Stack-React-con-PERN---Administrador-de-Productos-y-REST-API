9a22515c86c10768990f54372695cfe865489b65
Miguel Tafur <miguelmita03@gmail.com>
2025-09-20 11:10:22 -0300

    API enlazada con React

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
 import swaggerUI from 'swagger-ui-express'
 import swaggerSpec from './config/swagger'
 import router from './router'
 import db from './config/db'
+import morgan from 'morgan'
 
 // Conectar a base de datos
 export async function conectDB() {
@@ -22,6 +23,20 @@ conectDB()
 
 // Instancia de express
 const server = express()
+
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback){
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
+
+server.use(morgan('dev'))
 
 // Leer datos de formularios
 server.use(express.json())
